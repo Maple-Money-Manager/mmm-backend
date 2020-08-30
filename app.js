3a071e8f2005692f2f2b3f392182e49a4fc808c1
leechongren <lee_chongren@hotmail.com>
2020-08-30 10:39:05 +0800

    [#15][CR] install cors for backend and use as middleware in app

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:3001",
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
   res.json("Get is working");
 });
-
 const recordsRouter = require("./controllers/records.routes");
 
 app.use("/records", recordsRouter);
